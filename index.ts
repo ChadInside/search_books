b1197f2f064427ab436ed74ca9c86c57fe919079
Dmytro Savchuk <oyduy1@gmail.com>
2023-02-16 19:49:10 +0200

    bot

--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import { Context, Telegraf, Markup, Composer } from 'telegraf'
+import { Update } from 'typegram'
+import { useNewReplies } from 'telegraf/future'
+import { message } from 'telegraf/filters'
+import axios from 'axios'
+import dotenv from 'dotenv'
+dotenv.config()
+import superagent from 'superagent'
 
-dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_TOKEN)
+bot.use(useNewReplies())
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+// bot.use(Telegraf.log())
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+bot.hears(/.+/, async ctx => {
+  return ctx.reply(`Oh, ${ctx.match[0]}! Great choice`)
+})
+
+bot.catch(e => {
+  console.log('ERROR')
+  console.log(e)
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
