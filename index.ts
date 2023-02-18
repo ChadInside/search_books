a9d520d8ef99f1ca1e20c854f0a66f5b11ea9764
Dmytro Savchuk <oyduy1@gmail.com>
2023-02-18 13:04:57 +0200

    adding /download command handler

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import dotenv from 'dotenv'
 import Book from 'flibusta/build/types/book'
 import Author from 'flibusta/build/types/authors'
 import { randomInt } from 'crypto'
+import { BooksByName } from 'flibusta/build/types/booksByName'
 dotenv.config()
 
 const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_TOKEN)
@@ -16,39 +17,47 @@ const PAGE_SIZE = 5
 bot.use(useNewReplies())
 
 // bot.use(Telegraf.log())
+bot.hears('/(?<=^download)\d+$/', async ctx=>{
+//get info on book
+  const bookId : number= Number(ctx.match[0])
+  const book = await flibustaApi.getCoverByBookId
+  return ctx.reply('👍👍')
 
-bot.hears(/^.{1,3}$/, async ctx => {
-  return ctx.sendMessage(`Too short query`)
+
+
 })
 bot.hears(/.+/, async ctx => {
   const query: string = ctx.match[0]
-  console.log(query)
+  const currentPage = 1
 
   const books = (await flibustaApi.getBooksByName(query)) || []
-  // console.dir(books)\
 
-  const page = 1
-  const totalPages = Math.ceil(books.length / PAGE_SIZE)
-  const pageBooks = paginate(books, page)
-  // console.log(paginatedBooks.length)
-  const array = new Array(totalPages).fill('')
-  console.dir(array)
-  const pageButtons = getpageButtons(page, totalPages, query)
-  console.log(pageButtons)
+  const { replyHTML, paginationButtons } = composeReply(query, currentPage, books)
 
-  const response: string = pageBooks!.reduce((acc, bookAuthors) => {
-    acc += book2Html(bookAuthors.book, bookAuthors.authors)
-    return acc + '\n'
-  }, '')
-
-  return ctx.sendMessage(response === '' ? 'no response' : response, {
+  return ctx.sendMessage(replyHTML === '' ? 'no response' : replyHTML, {
     parse_mode: 'HTML',
-    ...Markup.inlineKeyboard(pageButtons),
+    ...Markup.inlineKeyboard(paginationButtons),
   })
 })
-bot.hears(/.+/, async ctx => {
-  return ctx.reply(`Oh, ${ctx.match[0]}! Great choice`)
+
+bot.action(/.+/, async (ctx, next) => {
+  const action: { page?: number; query?: string } = JSON.parse(ctx.match[0])
+  if (action.hasOwnProperty('page') && action.hasOwnProperty('query')) {
+    const currentPage = action.page as number
+    const query = action.query as string
+    const books = (await flibustaApi.getBooksByName(query)) || []
+
+    const { replyHTML, paginationButtons } = composeReply(query, currentPage, books)
+    return ctx.editMessageText(replyHTML === '' ? 'no response' : replyHTML, {
+      parse_mode: 'HTML',
+      ...Markup.inlineKeyboard(paginationButtons),
+    })
+  }
+
+  return ctx.reply('👍').then(() => next())
 })
+
+
 bot.catch(e => {
   console.log('ERROR')
   console.log(e)
@@ -59,6 +68,21 @@ bot.launch()
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+function composeReply(query: string, currentPage: number, books: Array<BooksByName>) {
+  const totalPages = Math.ceil(books.length / PAGE_SIZE)
+  const paginationButtons = getPaginationButtons(currentPage, totalPages, query)
+
+  const paginatedBooks = paginate(books, currentPage)
+  const replyHTML: string =
+    `Found ${books.length} books\n\n` +
+    paginatedBooks!.reduce((acc, bookAuthors) => {
+      acc += book2Html(bookAuthors.book, bookAuthors.authors)
+      return acc + '\n'
+    }, '')
+
+  return { replyHTML, paginationButtons }
+}
 
 function book2Html(book: Book, authors: Array<Author>) {
   /** 
@@ -78,31 +102,90 @@ function book2Html(book: Book, authors: Array<Author>) {
   return result
 }
 
-function paginate(books: Array<any>, page = 1, pageSize = PAGE_SIZE) {
-  return books.slice((page - 1) * pageSize, page * pageSize)
+function paginate<Type>(items: Array<Type>, page = 1, pageSize = PAGE_SIZE): Array<Type> {
+  return items.slice((page - 1) * pageSize, page * pageSize)
 }
 
-function getpageButtons(currentPage: number, totalPages: number, query: string) {
-  const currentPageIndicator = '·'
-  if (totalPages < 6) {
-    return new Array(totalPages).fill('').map((_, i) => {
-      if (i + 1 === currentPage) {
-        return Markup.button.callback(
-          `${currentPageIndicator} ${i + 1} ${currentPageIndicator}`,
-          `page:${i + 1}, query: ${query}`
-        )
-      }
-      return Markup.button.callback(`${i + 1}`, `page:${i + 1}, query: ${query}`)
-    })
+function getPaginationButtons(currentPage: number, totalPages: number, query: string) {
+  const pagesArray = parsePages(currentPage, totalPages)
+  const buttons = pagesArray.map(pageObj => {
+    const callback = JSON.stringify({ page: pageObj.page, query })
+    return Markup.button.callback(pageObj.buttonLabel, callback)
+  })
+  return buttons
+}
+
+function parsePages(currentPage: number, totalPages: number): Array<{ buttonLabel: string; page: number }> {
+  let result: Array<{ buttonLabel: string; page: number }> = [{ buttonLabel: '', page: 0 }]
+  if (totalPages <= 5) {
+    result = Array(totalPages)
+      .fill('')
+      .map((_, i) => ({ buttonLabel: wrapCurrentPage(currentPage, i + 1), page: i + 1 }))
   } else {
-    return new Array(totalPages).fill('').map((_, i) => {
-      if (i + 1 === currentPage) {
-        return Markup.button.callback(
-          `${currentPageIndicator} ${i + 1} ${currentPageIndicator}`,
-          `page:${i + 1}, query: ${query}`
-        )
-      }
-      return Markup.button.callback(`${i + 1}`, `page:${i + 1}, query: ${query}`)
-    })
+    if (currentPage <= 3)
+      result = [
+        { buttonLabel: wrapCurrentPage(currentPage, 1), page: 1 },
+        { buttonLabel: wrapCurrentPage(currentPage, 2), page: 2 },
+        { buttonLabel: wrapCurrentPage(currentPage, 3), page: 3 },
+        { buttonLabel: wrapNextPage(4), page: 4 },
+        { buttonLabel: wrapLastPage(totalPages), page: totalPages },
+      ]
+    if (currentPage >= 4 && currentPage <= totalPages - 3)
+      result = [
+        { buttonLabel: wrapFirstPage(), page: 1 },
+        { buttonLabel: wrapPreviousPage(currentPage - 1), page: currentPage - 1 },
+        { buttonLabel: wrapCurrentPage(currentPage, currentPage), page: currentPage },
+        { buttonLabel: wrapNextPage(currentPage + 1), page: currentPage + 1 },
+        { buttonLabel: wrapLastPage(totalPages), page: totalPages },
+      ]
+    if (currentPage >= totalPages - 2)
+      result = [
+        { buttonLabel: wrapFirstPage(), page: 1 },
+        { buttonLabel: wrapPreviousPage(totalPages - 3), page: totalPages - 3 },
+        { buttonLabel: wrapCurrentPage(currentPage, totalPages - 2), page: totalPages - 2 },
+        { buttonLabel: wrapCurrentPage(currentPage, totalPages - 1), page: totalPages - 1 },
+        { buttonLabel: wrapCurrentPage(currentPage, totalPages), page: totalPages },
+      ]
   }
+  return result
+}
+
+function testPageButtons() {
+  const MAX_BUTTONS = 5
+  const totalPagesArray = [1, 2, 3, 4, 5, 6, 7, 10, 25]
+  const currentPageArray = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 22, 23, 24, 25, 26, 27]
+
+  const result = []
+  for (const totalPages of totalPagesArray) {
+    for (const currentPage of currentPageArray) {
+      if (currentPage < 1 || currentPage > totalPages) {
+        // console.log(`not valid current: ${currentPage}, total: ${totalPages}`)
+        continue
+      }
+      result.push(parsePages(currentPage, totalPages))
+    }
+  }
+
+  console.log(result)
+}
+
+function wrapFirstPage() {
+  const firstPageSymbol = '<<'
+  return `${firstPageSymbol}1`
+}
+function wrapPreviousPage(pageNumber: number) {
+  const previousPageSymbol = '<'
+  return `${previousPageSymbol}${pageNumber}`
+}
+function wrapLastPage(pageNumber: number) {
+  const lastPageSymbol = '>>'
+  return `${pageNumber}${lastPageSymbol}`
+}
+function wrapNextPage(pageNumber: number) {
+  const nextPageSymbol = '>'
+  return `${pageNumber}${nextPageSymbol}`
+}
+function wrapCurrentPage(currentPage: number, page: number) {
+  const currentPageSymbol = '·'
+  return currentPage === page ? `${currentPageSymbol}${currentPage}${currentPageSymbol}` : `${page}`
 }
